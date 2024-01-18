129d44e4c4379bdc8b2761f37298c73a308d377b
jlokitha <ljanindulokitha@gmail.com>
2024-01-18 16:55:59 +0530

    Server Implemented

---
+++ src/main/java/lk/ijse/group_chat_app/ConnectionHandler.java
@@ -0,0 +1,60 @@
+package lk.ijse.group_chat_app;
+
+import java.io.*;
+import java.net.Socket;
+
+public class ConnectionHandler implements Runnable {
+    private Socket client;
+    private DataOutputStream out;
+    private BufferedReader in;
+    private String username;
+
+    public ConnectionHandler ( Socket client ) {
+        this.client = client;
+    }
+    @Override
+    public void run () {
+        try {
+            in = new BufferedReader ( new InputStreamReader ( client.getInputStream () ) );
+            out = new DataOutputStream ( client.getOutputStream () );
+
+            String message = in.readLine ();
+
+            while ( message != null ) {
+                if ( message.startsWith ( "Username" ) ) {
+                    String[] user = message.split ( " " );
+
+                    if ( user.length == 2 ) {
+                        username = user[1];
+                        Server.getServer ().broadcast ( user[1] + "Joined the chat !" );
+                    }
+                } else if (message.startsWith ( "Shutdown" )) {
+                    Server.getServer ().broadcast ( username + " left !" );
+                    shutdown ();
+                } else {
+                    Server.getServer ().broadcast ( username + ": " + message );
+                }
+            }
+        } catch ( IOException e ) {
+            e.printStackTrace ();
+        }
+    }
+
+    public void sendMessage(String message) {
+        try {
+            out.writeUTF ( message );
+        } catch ( IOException e ) {
+            e.printStackTrace ();
+        }
+    }
+
+    public void shutdown() {
+        try {
+            in.close ();
+            out.close ();
+            client.close ();
+        } catch ( IOException e ) {
+            e.printStackTrace ();
+        }
+    }
+}
--- src/main/java/lk/ijse/group_chat_app/Server.java
+++ src/main/java/lk/ijse/group_chat_app/Server.java
@@ -1,13 +1,52 @@
 package lk.ijse.group_chat_app;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Server implements Runnable {
+    private static Server server;
+    private ServerSocket serverSocket;
+    private ArrayList<ConnectionHandler> connectionList;
+    private ExecutorService threadPool;
 
-    public static void main ( String[] args ) {
-
+    private Server() {
+        connectionList = new ArrayList<> ();
     }
 
     @Override
     public void run () {
+        try {
+            serverSocket = new ServerSocket ( 5000 );
+            threadPool = Executors.newCachedThreadPool ( );
 
+            while ( true ) {
+                Socket client = serverSocket.accept ( );
+                System.out.println ( "A client connected !" );
+                ConnectionHandler connection = new ConnectionHandler ( client );
+                connectionList.add ( connection );
+                threadPool.execute ( connection );
+            }
+
+        } catch ( IOException e ) {
+            e.printStackTrace ();
+        }
+    }
+
+    public void broadcast(String message) {
+        for (ConnectionHandler client : connectionList) {
+            client.sendMessage ( message );
+        }
+    }
+
+    public static Server getServer() {
+        return (server == null) ? server = new Server () : server;
+    }
+
+    public static void main ( String[] args ) {
+        Server.getServer ().run ();
     }
 }
